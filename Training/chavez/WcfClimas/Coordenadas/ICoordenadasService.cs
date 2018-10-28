05c314ca3a927ab06bf322f7a50048bfab84691e
eduardo <e_eduardo_15@hotmail.com>
2018-10-28 14:54:35 -0500

    serviceClimas

---
+++ Training/chavez/WcfClimas/Coordenadas/ICoordenadasService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfClimas.Coordenadas
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "ICoordenadasService" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface ICoordenadasService
+    {
+        [OperationContract]
+        string ObtenerCoordenadas();
+    }
+}
