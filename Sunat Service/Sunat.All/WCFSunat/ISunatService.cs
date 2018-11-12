4b723f9594380085ab407d3fee83dfb453cbb380
Jaime Zamora <jzamora@aptos.com>
2018-11-11 21:59:24 -0500

    WCF Reniec

---
+++ Sunat Service/Sunat.All/WCFSunat/ISunatService.cs
@@ -0,0 +1,15 @@
+﻿using System.ServiceModel;
+using WCFSunat.Dominio;
+
+namespace WCFSunat
+{
+    [ServiceContract]
+    public interface ISunatService
+    {
+        [OperationContract]
+        bool EsDniValido(string dni);
+
+        [OperationContract]
+        Persona ObtenerPersonaPorDni(string dni);
+    }
+}
---
+++ Sunat Service/Sunat.All/WCFSunat/Mapeo/Mapper.cs
@@ -0,0 +1,18 @@
+﻿using Tecactus.Api.Reniec;
+using WCFSunat.Dominio;
+
+namespace WCFSunat.Mapeo
+{
+    public class Mapper
+    {
+        public Persona ConvertirPersonaApiAPersona(Person persona)
+        {
+            return new Persona() {
+                Dni = persona.dni,
+                Nombres = persona.nombres,
+                ApellidoPaterno = persona.apellido_paterno,
+                ApellidoMaterno = persona.apellido_materno
+            };
+        }
+    }
+}
