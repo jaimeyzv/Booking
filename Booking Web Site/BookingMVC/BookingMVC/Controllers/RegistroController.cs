f9da25075905483ecd1554809b3cbb8f0b478e95
Jaime Zamora <jzamora@aptos.com>
2018-11-12 21:54:48 -0500

    Html for signup

---
+++ Booking Web Site/BookingMVC/BookingMVC/Controllers/RegistroController.cs
@@ -0,0 +1,18 @@
+﻿using BookingMVC.Models;
+using System.Web.Mvc;
+
+namespace BookingMVC.Controllers
+{
+    public class RegistroController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Registrar(RegistroModel model)
+        {
+            return View();
+        }
+    }
+}
