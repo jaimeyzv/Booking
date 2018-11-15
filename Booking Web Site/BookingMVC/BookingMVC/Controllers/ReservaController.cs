015156df013bca413dfe2164161a5ece5583bce4
Eduardo Raul Rojas Huaman <nitratospeed@gmail.com>
2018-11-15 10:49:21 -0500

    reserva fix

---
+++ Booking Web Site/BookingMVC/BookingMVC/Controllers/ReservaController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookingMVC.Controllers
+{
+    public class ReservaController : Controller
+    {
+        // GET: Reserva
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: Reserva
+        //[HttpPost]
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
