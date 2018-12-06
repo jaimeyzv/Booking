515ab066d4dc34af73577cd609e8ee18e3fd8968
Rodrigo Salazar <rsalazart@hotmail.com>
2018-12-06 03:52:32 -0500

    Revert "cnxString backup"
    This reverts commit ce931789a52b0d337702d70c3aebc98047347cd1.

--- Booking Web Site/BookingMVC/Booking.DataAccess/BalanceApi.cs
+++ Booking Web Site/BookingMVC/Booking.DataAccess/BalanceApi.cs
@@ -7,7 +7,7 @@ namespace Booking.DataAccess
     {
         public Balances ObtenerSaldoPorCardId(int cardId)
         {
-            var client = new RestClient("http://localhost:9609/");
+            var client = new RestClient("http://localhost:91/");
             var request = new RestRequest("api/balance/{cardId}", Method.GET);
             request.AddParameter("cardId", cardId);
             var queryResult = client.Execute<Balances>(request).Data;
